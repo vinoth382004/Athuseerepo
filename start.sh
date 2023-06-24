db61113fecc5ac5b807e541e09da2016e8a0e096
Vanam2002 <117515657+Vanam2002@users.noreply.github.com>
2023-06-24 16:33:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vanam2002/DQ-the-file-donor-bot /DQTheFileDonorBot
+  git clone https://github.com/Vanam2002/DQ-the-file-donor-bot  /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
